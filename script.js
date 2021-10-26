c806dc6253993bd2003cab5b5d3b92f8e814252c
Dayana <dayanadiasgarcia@gmail.com>
2021-10-26 19:38:25 -0300

    Requisito 9

--- script.js
+++ script.js
@@ -5,22 +5,24 @@ let list = document.getElementById('lista-tarefas');//ol aqui
 function criaItem() {
   let itemLista = document.createElement('li');//li aqui
   list.appendChild(itemLista);//li filho de ol aqui
+
   itemLista.innerText = digita.value;// pega o texto do input
   digita.value = '';//apaga os valores do input
-  itemLista.addEventListener('click', addBackground);
   itemLista.addEventListener('dblclick', riscaTarefa);
-}
-
-function addBackground(event) {
- event.target.style.backgroundColor = 'rgb(128, 128, 128)';
+  itemLista.addEventListener('click', selecionado);
 }
 
 function riscaTarefa(event) {
-  event.target.style.textDecoration = 'line-through';
+  event.target.classList.add('completed');
 }
 
-function selecionado() {
+function selecionado(event) {
+  let selected = document.querySelectorAll('.selected');//cria var para selecionar
 
+  for (let index = 0; index < selected.length; index++) {
+    selected[index].classList.remove('selected');//remove todos os itens selecionados
+  }
+  event.target.classList.add('selected');// vai add a classe para o item que está clicado agora
 }
 
 btn.addEventListener('click', criaItem);
