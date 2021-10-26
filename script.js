f99a30e2867edee05276b0989979f073a291090e
Dayana <dayanadiasgarcia@gmail.com>
2021-10-26 17:23:24 -0300

    alteraçao no style.css

--- script.js
+++ script.js
@@ -6,7 +6,12 @@ function criaItem() {
   let itemLista = document.createElement('li');//li aqui
   list.appendChild(itemLista);//li filho de ol aqui
   itemLista.innerText = digita.value;// pega o texto do input
-  digita.value = '';
-};
+  digita.value = '';//apaga os valores do input
+}
+function addBackground() {
+  //itemLista.style.background = 'rgb(128, 128, 128)';
+  itemLista.style.backgroundColor = 'rgb(128, 128, 128)';
+}
 
 btn.addEventListener('click', criaItem);
+itemLista.addEventListener('click', addBackground);
