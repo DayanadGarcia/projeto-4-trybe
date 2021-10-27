839443add35b82472dd8ee96e8c4091e3a77eeda
Dayana <dayanadiasgarcia@gmail.com>
2021-10-27 16:36:14 -0300

    Requisito 11

--- script.js
+++ script.js
@@ -1,39 +1,40 @@
-let digita = document.getElementById('texto-tarefa');//input aqui
-let btn = document.getElementById('criar-tarefa');//botão add aqui
-let list = document.getElementById('lista-tarefas');//ol aqui
-let btnApaga = document.getElementById('apaga-tudo');//botao de apagar aqui
+const digita = document.getElementById('texto-tarefa'); // input aqui
+const btnCriar = document.getElementById('criar-tarefa'); // botão add aqui
+const list = document.getElementById('lista-tarefas'); // ol aqui
+const btnApgTudo = document.getElementById('apaga-tudo'); // botao de apagar tudo aqui
+const btnApgFinalizados = document.getElementById('remover-finalizados'); // botao de apagar finalizados
 
-function criaItem() {
-  let itemLista = document.createElement('li');//li aqui
-  list.appendChild(itemLista);//li filho de ol aqui
-  itemLista.innerText = digita.value;// pega o texto do input
-  digita.value = '';//apaga os valores do input
-  itemLista.addEventListener('click', selecionado);
-  itemLista.addEventListener('dblclick', riscaTarefa); 
-}
-
-function riscaTarefa(event) {//marca a tarefa como concluida
-  let acionado = document.querySelector('.completed'); //variavel que guarda a class completed "ativa"
-  if (acionado) { //compara se possui a class completed
-    event.target.classList.remove('completed');//remove a class completed
-  }else if(!acionado) {//verifica se não tem a classe
-    event.target.classList.add('completed');//add a classe
+function riscaTarefa(event) { // ----------funcao RiscaTarefa----------
+  if (event.target.classList.contains('completed')) { // compara se contem a class completed
+    event.target.classList.remove('completed'); // remove a class completed
+  } else { // verifica se não tem a classe
+    event.target.classList.add('completed'); // add a classe
   }
 }
+btnCriar.addEventListener('click', () => { // -----------cria elementos na lista-----------
+  const itemLista = document.createElement('li'); // li aqui
+  list.appendChild(itemLista); // li filho de ol aqui
+  itemLista.innerText = digita.value; // pega o texto do input
+  digita.value = ''; // apaga os valores do input
+  itemLista.addEventListener('click', (event) => { // -------funçao anonima selecionado()-------
+    const selected = document.querySelectorAll('.selected'); // cria var para selecionar
+    for (let index = 0; index < selected.length; index += 1) {
+      selected[index].classList.remove('selected'); // remove todos os itens selecionados
+    }
+    event.target.classList.add('selected'); // vai add a classe para o item que está clicado agora
+  });
+  itemLista.addEventListener('dblclick', riscaTarefa); // chama a funcao riscaTarefa ^
+});
 
-function selecionado(event) {
-  let selected = document.querySelectorAll('.selected');//cria var para selecionar
-  for (let index = 0; index < selected.length; index++) {
-    selected[index].classList.remove('selected');//remove todos os itens selecionados
+btnApgTudo.addEventListener('click', () => { // ------funçao apagaTudo()---------
+  const tarefas = document.querySelector('ol'); // seleciona a lista ordenada
+  tarefas.innerHTML = ''; // transforma seu conteudo em vazio
+});
+
+btnApgFinalizados.addEventListener('click', () => { // -----------funcao apagaConcluido-------------
+  const concluido = document.querySelectorAll('.completed');
+  for (let index = 0; index < concluido.length; index += 1) {
+    concluido[index].remove('completed');
   }
-  event.target.classList.add('selected');// vai add a classe para o item que está clicado agora
-}
-
-function apagaTudo(event) {
-  let tarefas = document.querySelector("ol");//seleciona a lista ordenada
-  tarefas.innerHTML = "";//transforma seu conteudo em vazio
-  
-}
-
-btn.addEventListener('click', criaItem);
-btnApaga.addEventListener('click', apagaTudo);
+  // event.target.classList.remove('completed');
+});
